3c7ae643767b4e05bba3eb499f14128f37b118dd
Bill Thornton <billt2006@gmail.com>
2020-06-18 12:16:33 -0400

    Add expo-keep-awake to dependencies

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake';
+import { activateKeepAwake } from 'expo-keep-awake';
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { ThemeProvider } from 'react-native-elements';
