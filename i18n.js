cb3cde82a2448713c4d483e4c93a956ec289f9b6
Bill Thornton <billt2006@gmail.com>
2020-07-14 23:57:03 -0400

    Add stub files for Spanish and French

--- i18n.js
+++ i18n.js
@@ -3,9 +3,13 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './langs/en.json';
+import es from './langs/es.json';
+import fr from './langs/fr.json';
 
 const resources = {
-  en: { translation: en }
+  en: { translation: en },
+  es: { translation: es },
+  fr: { translation: fr }
 };
 
 i18next
