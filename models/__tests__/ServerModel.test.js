195dff150dd74c5b88dc56e6f6fe96e3906c3ea0
Bill Thornton <billt2006@gmail.com>
2021-02-25 01:08:55 -0500

    Add test for ServerModel

---
+++ models/__tests__/ServerModel.test.js
@@ -0,0 +1,70 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import Url from 'url';
+
+import ServerModel from '../ServerModel';
+
+describe('ServerModel', () => {
+	beforeEach(() => {
+		fetch.resetMocks();
+		jest.clearAllMocks();
+	});
+
+	it('should create a ServerModel with computed properties', () => {
+		const server = new ServerModel(
+			'testId',
+			Url.parse('https://foobar'),
+			{
+				ServerName: 'Test Server'
+			}
+		);
+
+		expect(server.id).toBe('testId');
+		expect(server.online).toBe(false);
+		expect(server.urlString).toBe('https://foobar/');
+		expect(server.name).toBe('Test Server');
+	});
+
+	it('should fallback to the url if server name is unavailable', () => {
+		const server = new ServerModel(
+			'testId',
+			Url.parse('https://foobar')
+		);
+
+		expect(server.name).toBe('foobar');
+	});
+
+	it('should fallback to an empty string when the url is invalid', () => {
+		const server = new ServerModel('testId');
+
+		expect(server.urlString).toBe('');
+	});
+
+	it('should set the online status and info when fetchInfo is called', async () => {
+		fetch.mockResponse(JSON.stringify({ ServerName: 'Test Server' }));
+
+		const server = new ServerModel(
+			'testId',
+			Url.parse('https://foobar')
+		);
+		await server.fetchInfo();
+
+		expect(server.online).toBe(true);
+		expect(server.name).toBe('Test Server');
+	});
+
+	it('should keep the offline status when fetchInfo fails', async () => {
+		fetch.mockResponse(JSON.stringify({ error: 'test' }), { status: 500 });
+
+		const server = new ServerModel(
+			'testId',
+			Url.parse('https://foobar')
+		);
+		await server.fetchInfo();
+
+		expect(server.online).toBe(false);
+	});
+});
