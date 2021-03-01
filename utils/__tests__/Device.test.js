0e5ab04122b0e1d31dfad5ada5e3ce44f064b08e
Bill Thornton <billt2006@gmail.com>
2021-02-28 20:56:55 -0500

    Add tests for getDeviceProfile

--- utils/__tests__/Device.test.js
+++ utils/__tests__/Device.test.js
@@ -4,10 +4,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
-import { getAppName, getSafeDeviceName } from '../Device';
+import { getAppName, getDeviceProfile, getSafeDeviceName } from '../Device';
+import iOS10Profile from '../profiles/ios10';
+import iOS12Profile from '../profiles/ios12';
+import iOSProfile from '../profiles/ios';
+
+jest.mock('react-native/Libraries/Utilities/Platform');
 
 describe('Device', () => {
+	beforeEach(() => {
+		jest.resetModules();
+	});
+
 	describe('getAppName()', () => {
 		it('should return the app name including the os name', () => {
 			expect(getAppName()).toBe('Jellyfin Mobile (mock)');
@@ -32,6 +42,33 @@ describe('Device', () => {
 		it('should return a default name if device name only contains non-ascii and whitespace characters', () => {
 			Constants.deviceName = '  🌮😂 ';
 			expect(getSafeDeviceName()).toBe('Jellyfin Mobile Device');
+		});
+	});
+
+	describe('getDeviceProfile()', () => {
+		it('should return the correct profile for iOS 10 devices', () => {
+			Platform.Version = '10';
+			expect(getDeviceProfile()).toBe(iOS10Profile);
+		});
+
+		it('should return the correct profile for iOS 11 devices', () => {
+			Platform.Version = '11';
+			expect(getDeviceProfile()).toBe(iOS12Profile);
+		});
+
+		it('should return the correct profile for iOS 12 devices', () => {
+			Platform.Version = '12';
+			expect(getDeviceProfile()).toBe(iOS12Profile);
+		});
+
+		it('should return the correct profile for iOS 13 devices', () => {
+			Platform.Version = '13';
+			expect(getDeviceProfile()).toBe(iOSProfile);
+		});
+
+		it('should return the an empty profile for Android devices', () => {
+			Platform.OS = 'android';
+			expect(getDeviceProfile()).toStrictEqual({});
 		});
 	});
 });
