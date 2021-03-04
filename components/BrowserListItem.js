bbc253f50013b56010ba1ecc1fb936da4462ac51
Bill Thornton <billt2006@gmail.com>
2021-03-04 10:36:01 -0500

    Add test for BrowserListItem

--- components/BrowserListItem.js
+++ components/BrowserListItem.js
@@ -3,23 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React from 'react';
 import { Icon, ListItem } from 'react-native-elements';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 import { openBrowser } from '../utils/WebBrowser';
 
 const BrowserListItem = ({ item, index }) => (
 	<ListItem
+		testID='browser-list-item'
 		topDivider={index === 0}
 		bottomDivider
 		onPress={() => {
 			openBrowser(item.url);
 		}}
 	>
-		<Icon name={item.icon.name} type={item.icon.type} />
+		<Icon testID='icon' name={item.icon.name} type={item.icon.type} />
 		<ListItem.Content>
-			<ListItem.Title>{item.name}</ListItem.Title>
+			<ListItem.Title testID='title'>{item.name}</ListItem.Title>
 		</ListItem.Content>
 		<ListItem.Chevron />
 	</ListItem>
