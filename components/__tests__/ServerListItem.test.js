573c439dc4206305d1fb4484d14a7227d10bc648
Bill Thornton <billt2006@gmail.com>
2021-03-19 11:08:48 -0400

    Add test for ServerListItem

---
+++ components/__tests__/ServerListItem.test.js
@@ -0,0 +1,60 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { fireEvent, render } from '@testing-library/react-native';
+import React from 'react';
+
+import ServerListItem from '../ServerListItem';
+import '../../i18n';
+
+describe('ServerListItem', () => {
+	it('should render correctly and handle presses', () => {
+		const onDelete = jest.fn();
+		const onPress = jest.fn();
+
+		const { getByTestId, queryByTestId } = render(
+			<ServerListItem
+				item={{
+					name: 'Test Server',
+					info: {
+						Version: '10.0.0'
+					},
+					urlString: 'https://foobar'
+				}}
+				index={0}
+				activeServer={0}
+				onDelete={onDelete}
+				onPress={onPress}
+			/>
+		);
+
+		expect(queryByTestId('active-icon')).not.toBeNull();
+
+		expect(getByTestId('title')).toHaveTextContent('Test Server');
+		expect(getByTestId('subtitle')).toHaveTextContent('Version: 10.0.0 https://foobar');
+
+		expect(onDelete).not.toHaveBeenCalled();
+		fireEvent.press(getByTestId('delete-button'));
+		expect(onDelete).toHaveBeenCalledWith(0);
+
+		expect(onPress).not.toHaveBeenCalled();
+		fireEvent.press(getByTestId('server-list-item'));
+		expect(onPress).toHaveBeenCalledWith(0);
+	});
+
+	it('should not render active icon for inactive server', () => {
+		const { queryByTestId } = render(
+			<ServerListItem
+				item={{}}
+				index={0}
+				activeServer={1}
+				onDelete={() => {}}
+				onPress={() => {}}
+			/>
+		);
+
+		expect(queryByTestId('active-icon')).toBeNull();
+	});
+});
