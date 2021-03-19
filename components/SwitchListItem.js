39deacf698212ffa1f131375ee51f477d2cd5d42
Bill Thornton <billt2006@gmail.com>
2021-03-19 11:38:03 -0400

    Add test for SwitchListItem

--- components/SwitchListItem.js
+++ components/SwitchListItem.js
@@ -3,24 +3,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React from 'react';
-import { Switch } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { Switch } from 'react-native';
 
 const SwitchListItem = ({ item, index }) => (
 	<ListItem
+		testID='switch-list-item'
 		topDivider={index === 0}
 		bottomDivider
 	>
 		<ListItem.Content>
-			<ListItem.Title>{item.title}</ListItem.Title>
+			<ListItem.Title
+				testID='title'
+			>
+				{item.title}
+			</ListItem.Title>
 			{(
 				item.subtitle &&
-				<ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
+				<ListItem.Subtitle
+					testID='subtitle'
+				>
+					{item.subtitle}
+				</ListItem.Subtitle>
 			)}
 		</ListItem.Content>
 		<Switch
+			testID='switch'
 			disabled={item.disabled}
 			value={item.value}
 			onValueChange={item.onValueChange}
