21cf206cfbcf711d09e387b51ca3ddda15c9fa88
Bill Thornton <billt2006@gmail.com>
2021-11-04 15:11:45 -0400

    Add Links test

---
+++ constants/__tests__/Links.test.js
@@ -0,0 +1,14 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { QuickStartUrl } from '../Links';
+
+// I don't understand why jest thinks this needs test coverage, but it does =/
+describe('Links', () => {
+	it('should return the right url', () => {
+		expect(QuickStartUrl).toBe('https://jellyfin.org/docs/general/quick-start.html');
+	});
+});
