ac122d4c263c966271de4bccdfb5ca5b1951f352
Bill Thornton <billt2006@gmail.com>
2021-11-04 14:30:37 -0400

    Add RefreshWebView test

---
+++ components/__tests__/RefreshWebView.test.js
@@ -0,0 +1,24 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { render } from '@testing-library/react-native';
+import React from 'react';
+
+import RefreshWebView from '../RefreshWebView';
+
+// NOTE: This test just verifies the component renders, because the
+// functionality would be very difficult to test properly
+describe('RefreshWebView', () => {
+	it('should render', () => {
+		const didRender = render(
+			<RefreshWebView
+				isRefreshing={false}
+			/>
+		);
+
+		expect(didRender).toBeTruthy();
+	});
+});
