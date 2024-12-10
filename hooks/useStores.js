f9e09bdc9d441d01209f06e8c19d4cdcf89d21ad
enigma0Z <enigma.0ZA@gmail.com>
2024-12-10 15:04:23 -0500

    Reference new zustand root store

--- hooks/useStores.js
+++ hooks/useStores.js
@@ -3,12 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { createContext, useContext } from 'react';
+import { useRootStore } from '../stores/RootStore';
 
-import RootStore from '../stores/RootStore';
-
-export const storesContext = createContext({
-	rootStore: new RootStore()
-});
-
-export const useStores = () => useContext(storesContext);
+// Compatibility for zustand conversion
+export const useStores = () => { return { rootStore: useRootStore() } }
