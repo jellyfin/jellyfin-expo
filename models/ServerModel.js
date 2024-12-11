bc387f4d5a6841cb844ebe5a7368903e64bd659f
enigma0Z <enigma.0ZA@gmail.com>
2024-12-11 09:00:30 -0500

    Remove mobx action implementations

--- models/ServerModel.js
+++ models/ServerModel.js
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { action, autorun, computed, decorate, observable } from 'mobx';
-import { task } from 'mobx-task';
-
 import { fetchServerInfo, getServerUrl } from '../utils/ServerValidator';
 
 export default class ServerModel {
